63c214841dc418b5ca2d5490fb3ebbfad06db514
kimanimargaret46@gmail.com <kimanimargaret46@gmail.com>
2020-08-02 12:48:08 +0300

    heroku

--- gallery/settings.py
+++ gallery/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 from decouple import config, Csv
 import dj_database_url
-import django_heroku
+# import django_heroku
 
 
 MODE=config("MODE", default="dev")
